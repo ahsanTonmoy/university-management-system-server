37533650f663145bde9bcf803c12b15545bd0ff4
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2025-08-22 20:17:08 +0600

    create data model

---
+++ src/app/modules/users/user.model.ts
@@ -0,0 +1,44 @@
+import { model, Schema } from "mongoose";
+import { IUser } from "./user.interface";
+
+const userSchema = new Schema<IUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: false,
+    },
+    role: {
+      type: String,
+      enum: ['superAdmin', 'admin', 'student', 'faculty'],
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+      default: 'in-progress',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true, // adds createdAt and updatedAt
+  }
+);
+
+export const UserModel = model<IUser>('User', userSchema);
