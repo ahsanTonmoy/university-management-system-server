ec8c61a7d44a8b54c85ebc122c0d52f88bc258f6
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-05-13 23:42:41 +0600

    implement globalErrorHandler

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,13 @@
+// global err
+import { NextFunction, Request, Response } from 'express';
+
+export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+  // console.error(err.stack);
+  let statusCode = 500;
+  let message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    message,
+    error: err,
+  });
+};
