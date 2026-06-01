79063c1d05b32e26eaf802b7be9904eba7ef683d
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-06-01 23:57:43 +0600

    clean code

--- src/app/modules/users/user.controlar.ts
+++ src/app/modules/users/user.controlar.ts
@@ -1,13 +1,11 @@
 import { sendResponse } from "../../utils/sendResponce";
+import { createStudentZodSchema } from "../student/studentVelidetion";
 import { userService } from "./user.service";
-import { UserValidationSchema } from "./user.validator";
 import { NextFunction, Request, Response } from "express";
 import  HttpStatus  from 'http-status';
 const createStudent = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        // const validatedData = await UserValidationSchema.parseAsync(req.body);
         const {password, student: studentData} = req.body;
-    
         const result = await userService.createStudent(password, studentData);
         sendResponse(res, {
             statusCode: HttpStatus.OK,
--- src/app/modules/users/user.validator.ts
+++ src/app/modules/users/user.validator.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 export const UserValidationSchema = z.object({
   id: z.string().min(1, 'User ID is required'),
-  email: z.string().email('Invalid email address'),
   password: z.string().min(6, 'Password must be at least 6 characters'),
   needsPasswordChange: z.boolean(),
   passwordChangedAt: z.coerce.date().optional(), // Accepts string or Date
