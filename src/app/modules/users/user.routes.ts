6e7d4d8991435e96669f3579be8051393b20b10f
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-06-01 23:56:54 +0600

    add middleware

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -1,9 +1,11 @@
 
 import express from 'express';
 import { userController } from './user.controlar';
+import { validateRequest } from '../../middlewares/valiedeRequest';
+import { createStudentZodSchema } from '../student/studentVelidetion';
 const router = express.Router();
 // create a new user with student role
-router.post('/create-student', userController.createStudent);
+router.post('/create-student', validateRequest(createStudentZodSchema), userController.createStudent);
 // get all users
 router.get('/all-users', userController.getUsers);
 
