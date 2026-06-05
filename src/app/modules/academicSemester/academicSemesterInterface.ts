b6ac615f60e03282f042fb180fb5fea51df7b544
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-06-05 23:08:53 +0600

    ca\\

--- src/app/modules/academicSemester/academicSemesterInterface.ts
+++ src/app/modules/academicSemester/academicSemesterInterface.ts
@@ -1,9 +1,11 @@
-// create academic semester interface here
-export interface IAcademicSemester {
-  title: string;
-    year: number;
-    code: string;
-    startMonth: string;
-    endMonth: string;
-    
-}
+export type AcademicSemesterTitle = 'Autumn' | 'Summer' | 'Fall';
+export type AcademicSemesterCode = '01' | '02' | '03';
+export type AcademicSemesterMonth = 'January' | 'February' | 'March' | 'April' | 'May' | 'June' | 'July' | 'August' | 'September' | 'October' | 'November' | 'December';
+// academic semester interface
+export type TacademicSemester ={
+    title: AcademicSemesterTitle;
+    code: AcademicSemesterCode;
+    year: string;
+    startMonth: AcademicSemesterMonth;
+    endMonth: AcademicSemesterMonth;
+};
