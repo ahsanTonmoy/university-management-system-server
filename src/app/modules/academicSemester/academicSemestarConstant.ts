489bd813e085186da7acedc2cfd431c2d74458f0
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-06-05 23:09:53 +0600

    in

---
+++ src/app/modules/academicSemester/academicSemestarConstant.ts
@@ -0,0 +1,17 @@
+// academic semester constants
+export const academicSemesterTitle = ['Autumn', 'Summer', 'Fall'];
+export const academicSemesterCode = ['01', '02', '03'];
+export const academicSemesterMonth = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
