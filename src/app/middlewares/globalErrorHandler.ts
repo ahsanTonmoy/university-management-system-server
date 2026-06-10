ede6fc6330d59a51891b47dc3b0ce79fba780966
Ahsanur Rahman Tonmoy <ahosant82@gmail.com>
2026-06-10 21:19:32 +0600

    implement appError functionalty for hendle errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
   // console.error(err.stack);
-  let statusCode = 500;
+  let statusCode = err.statusCode ||500;
   let message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
